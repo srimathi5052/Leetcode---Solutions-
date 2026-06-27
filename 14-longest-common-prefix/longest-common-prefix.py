371f54b14fe01d189ffd8696b3ba571a7d2a7651
srimathi5052 <srimathi5052@gmail.com>
2026-06-27 20:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (33.73%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for s in strs[1:]:
+            while s[:len(prefix)] != prefix:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+
+        return prefix
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
