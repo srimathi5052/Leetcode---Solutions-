f1070a57b50c52d4c679e9996099b8a345e20967
srimathi5052 <srimathi5052@gmail.com>
2026-06-30 20:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (38.63%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+       
+        nums.sort()
+        result = []
+
+        def backtrack(start, path):
+            result.append(path[:])
+
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return result 
