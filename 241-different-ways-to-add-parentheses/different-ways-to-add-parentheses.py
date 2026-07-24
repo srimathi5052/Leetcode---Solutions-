1d48864480a16b5585ad2dbfd5de6a6bcec0fd7e
srimathi5052 <srimathi5052@gmail.com>
2026-07-24 21:26:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (46.12%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def diffWaysToCompute(self, expression):
+        """
+        :type expression: str
+        :rtype: List[int]
+        """
+        
+        memo = {}
+
+        def solve(expr):
+            if expr in memo:
+                return memo[expr]
+
+            res = []
+
+            for i, ch in enumerate(expr):
+                if ch in "+-*":
+                    left = solve(expr[:i])
+                    right = solve(expr[i + 1:])
+
+                    for l in left:
+                        for r in right:
+                            if ch == "+":
+                                res.append(l + r)
+                            elif ch == "-":
+                                res.append(l - r)
+                            else:
+                                res.append(l * r)
+
+            if not res:
+                res.append(int(expr))
+
+            memo[expr] = res
+            return res
+
+        return solve(expression)
