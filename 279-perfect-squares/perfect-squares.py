30ba11e1b33c561d27080e55a5a89a0addda5a6b
srimathi5052 <srimathi5052@gmail.com>
2026-07-28 21:30:44 +0530

    Time: 3119 ms (64.62%) | Memory: 12.5 MB (61.20%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        dp = [0] + [float('inf')] * n
+
+        for i in range(1, n + 1):
+            j = 1
+            while j * j <= i:
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
+                j += 1
+
+        return dp[n]
