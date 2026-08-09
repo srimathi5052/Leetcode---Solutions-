887600ec139b835e32c156a26d88d8c0f97bfa2a
srimathi5052 <srimathi5052@gmail.com>
2026-08-09 21:20:12 +0530

    Time: 1431 ms (7.18%) | Memory: 43.6 MB (39.13%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -5,21 +5,24 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-
+       
         m, n = len(s), len(p)
 
         dp = [[False] * (n + 1) for _ in range(m + 1)]
         dp[0][0] = True
 
+        # Empty string vs pattern containing only *
         for j in range(1, n + 1):
             if p[j - 1] == '*':
                 dp[0][j] = dp[0][j - 1]
 
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                if p[j - 1] == s[i - 1] or p[j - 1] == '?':
+
+                if p[j - 1] == '?' or p[j - 1] == s[i - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
+
                 elif p[j - 1] == '*':
-                    dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
 
         return dp[m][n]
