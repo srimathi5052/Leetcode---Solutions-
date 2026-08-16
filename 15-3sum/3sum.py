15f63b8af817db807acc88b83cbd5f0f4e4389e0
srimathi5052 <srimathi5052@gmail.com>
2026-08-16 20:59:24 +0530

    Time: 753 ms (60.03%) | Memory: 18.3 MB (58.89%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,5 +1,9 @@
 class Solution(object):
     def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         nums.sort()
         result = []
 
@@ -16,13 +20,14 @@ class Solution(object):
                 if total == 0:
                     result.append([nums[i], nums[left], nums[right]])
 
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-
                     left += 1
                     right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
 
                 elif total < 0:
                     left += 1
@@ -30,8 +35,3 @@ class Solution(object):
                     right -= 1
 
         return result
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        
