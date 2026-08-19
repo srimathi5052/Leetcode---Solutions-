4c6a226d62bb89b225e9f8f6d543e913a74a3b49
srimathi5052 <srimathi5052@gmail.com>
2026-08-19 09:56:25 +0530

    Time: 55 ms (47.16%) | Memory: 20.4 MB (46.37%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -13,14 +13,6 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
         while root:
             if p.val < root.val and q.val < root.val:
                 root = root.left
