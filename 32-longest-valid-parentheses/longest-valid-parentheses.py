c42d55b884618be532a52710ab82f7641bb2e314
srimathi5052 <srimathi5052@gmail.com>
2026-09-04 19:42:11 +0530

    Time: 17 ms (86.79%) | Memory: 13.6 MB (36.03%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -18,7 +18,7 @@ class Solution(object):
                 else:
                     max_len = max(max_len, i - stack[-1])
 
-        return max_len 
+        return max_len
         
         
         
